844ac3133271bd60b24146fcef64f78f73f75937
F4rab1 <issafarabi2022@gmail.com>
2025-04-01 23:41:02 +0200

    Example of Dependency Injection with Constructor

---
+++ src/main/java/com/farabi/store/OrderService.java
@@ -0,0 +1,14 @@
+package com.farabi.store;
+
+public class OrderService {
+
+    public PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder() {
+        paymentService.processPayment(10);
+    }
+}
