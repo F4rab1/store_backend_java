d69eabaa10d46e58301e4871a2ffe45532e4e9d9
F4rab1 <issafarabi2022@gmail.com>
2025-04-02 22:41:40 +0200

    IoC Container and Configuring Beans using Annotations

--- src/main/java/com/farabi/store/OrderService.java
+++ src/main/java/com/farabi/store/OrderService.java
@@ -1,18 +1,25 @@
 package com.farabi.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 
     public PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService, int x) {}
+
+    @Autowired
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
     }
 
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 }
