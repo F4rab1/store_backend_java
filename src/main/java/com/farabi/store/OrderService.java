91a2f5f1a59f19bb0c2819ef49e24f7307bf4378
F4rab1 <issafarabi2022@gmail.com>
2025-04-02 17:13:32 +0200

    Example of Dependency Injection with Setter

--- src/main/java/com/farabi/store/OrderService.java
+++ src/main/java/com/farabi/store/OrderService.java
@@ -4,11 +4,15 @@ public class OrderService {
 
     public PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
