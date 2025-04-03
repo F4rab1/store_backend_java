f78268fdd44face6d7865bb3057a13018cab824d
F4rab1 <issafarabi2022@gmail.com>
2025-04-03 20:44:27 +0200

    Simple example of Configuring Beans Using Code

--- src/main/java/com/farabi/store/OrderService.java
+++ src/main/java/com/farabi/store/OrderService.java
@@ -4,15 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class OrderService {
 
     public PaymentService paymentService;
 
     public OrderService(PaymentService paymentService, int x) {}
 
-    @Autowired
-    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
