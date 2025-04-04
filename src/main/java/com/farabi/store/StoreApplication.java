81352ce5c810f26b408c25767a3d16bff35aa8a1
F4rab1 <issafarabi2022@gmail.com>
2025-04-04 18:42:05 +0200

    Database connection

--- src/main/java/com/farabi/store/StoreApplication.java
+++ src/main/java/com/farabi/store/StoreApplication.java
@@ -9,14 +9,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
 
     public  static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
         var orderService = context.getBean(OrderService.class);
         var orderService2 = context.getBean(OrderService.class);
         var manager = context.getBean(NotificationManager.class);
         var resource = context.getBean(HeavyResource.class);
         orderService.placeOrder();
         manager.sendNotification("Order Placed");
-        context.close();
     }
 
 }
