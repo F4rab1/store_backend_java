0669a0ef7c21e87b8a2269e63936f25c0fdaf915
F4rab1 <issafarabi2022@gmail.com>
2025-05-01 23:59:49 +0500

    Repository examples

--- src/main/java/com/farabi/store/StoreApplication.java
+++ src/main/java/com/farabi/store/StoreApplication.java
@@ -1,56 +1,31 @@
 package com.farabi.store;
 
+import com.farabi.store.entities.User;
+import com.farabi.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
-//        //      One to Many Relationship
-//        var user = User.builder()
-//                .name("Farabi")
-//                .password("Farabi")
-//                .email("farabi@farabi.com")
-//                .build();
-//
-//        var address = Address.builder()
-//                .street("street")
-//                .city("city")
-//                .state("state")
-//                .zip("zip")
-//                .build();
-//
-//        user.addAddress(address);
-//        System.out.println(user);
-//
-//        //      Many to Many Relationship
-//        var user2 = User.builder()
-//                .name("Farabi")
-//                .password("Farabi")
-//                .email("farabi@farabi.com")
-//                .build();
-//
-//        user2.addTag("tag1");
-//
-//        System.out.println(user2);
-//
-//        //      One to One Relationship
-//        var user3 = User.builder()
-//                .name("Farabi")
-//                .password("Farabi")
-//                .email("farabi@farabi.com")
-//                .build();
-//
-//        var profile = Profile.builder()
-//                .bio("profile")
-//                .build();
-//
-//        user3.setProfile(profile);
-//        profile.setUser(user3);
-//
-//        System.out.println(user3);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var repository =  context.getBean(UserRepository.class);
+
+//      Create
+        var user = User.builder()
+                .name("Farabi")
+                .email("farabi@farabi.com")
+                .password("farabi")
+                .build();
+        var saved = repository.save(user);
+        System.out.println("Saved: " + saved);
+
+        repository.findById(1L).orElseThrow();
+        repository.findAll().forEach(System.out::println);
+
+        repository.deleteById(1L);
     }
 
 }
