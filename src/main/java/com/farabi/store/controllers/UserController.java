4d51210bb71f3b883d0a3b17c475fad3351ed070
F4rab1 <issafarabi2022@gmail.com>
2025-08-21 20:27:11 +0500

    User Dto

--- src/main/java/com/farabi/store/controllers/UserController.java
+++ src/main/java/com/farabi/store/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.farabi.store.controllers;
 
-import com.farabi.store.entities.User;
+import com.farabi.store.dtos.UserDto;
 import com.farabi.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +18,22 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        var users = userRepository.findAll();
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        var users = userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
---
+++ src/main/java/com/farabi/store/dtos/UserDto.java
@@ -0,0 +1,12 @@
+package com.farabi.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+}
