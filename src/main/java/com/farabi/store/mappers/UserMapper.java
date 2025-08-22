dea189cf6f0c7b12fce9cb1041e4f2034b680a6f
F4rab1 <issafarabi2022@gmail.com>
2025-08-22 16:33:11 +0500

    Customizing Response Data

--- src/main/java/com/farabi/store/mappers/UserMapper.java
+++ src/main/java/com/farabi/store/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.farabi.store.mappers;
 import com.farabi.store.dtos.UserDto;
 import com.farabi.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+//    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
