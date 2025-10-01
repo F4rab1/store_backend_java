68ed9129f5fa11b700723f223bf40c2dce6e8999
F4rab1 <issafarabi2022@gmail.com>
2025-10-01 15:32:18 +0500

    Managing Secrets

--- src/main/java/com/farabi/store/services/JwtService.java
+++ src/main/java/com/farabi/store/services/JwtService.java
@@ -2,12 +2,16 @@ package com.farabi.store.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
     public String generatetoken(String email) {
         final long tokenExpiration = 86400; // 1 day in seconds
 
@@ -15,7 +19,7 @@ public class JwtService {
             .subject(email)
             .issuedAt(new Date())
             .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-            .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
             .compact();
     }
 }
